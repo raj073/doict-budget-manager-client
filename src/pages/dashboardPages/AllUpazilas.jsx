27142e1ce3bb05104ac9ccc229d2e490818bad8b
raj073 <rajoni.das@outlook.com>
2024-11-07 00:58:08 +0600

    allUpazilaBudgetDataTable updated

--- src/pages/dashboardPages/AllUpazilas.jsx
+++ src/pages/dashboardPages/AllUpazilas.jsx
@@ -1,46 +1,123 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+import {
+  MaterialReactTable,
+  useMaterialReactTable,
+} from "material-react-table";
 import useAxiosPublic from "../../hooks/useAxios";
 
+//nested data is ok, see accessorKeys in ColumnDef below
+const data = [
+  {
+    name: {
+      firstName: "John",
+      lastName: "Doe",
+    },
+    address: "261 Erdman Ford",
+    city: "East Daphne",
+    state: "Kentucky",
+  },
+  {
+    name: {
+      firstName: "Jane",
+      lastName: "Doe",
+    },
+    address: "769 Dominic Grove",
+    city: "Columbus",
+    state: "Ohio",
+  },
+  {
+    name: {
+      firstName: "Joe",
+      lastName: "Doe",
+    },
+    address: "566 Brakus Inlet",
+    city: "South Linda",
+    state: "West Virginia",
+  },
+  {
+    name: {
+      firstName: "Kevin",
+      lastName: "Vandy",
+    },
+    address: "722 Emie Stream",
+    city: "Lincoln",
+    state: "Nebraska",
+  },
+  {
+    name: {
+      firstName: "Joshua",
+      lastName: "Rolluffs",
+    },
+    address: "32188 Larkin Turnpike",
+    city: "Charleston",
+    state: "South Carolina",
+  },
+];
+
 const AllUpazilas = () => {
-  const [upazilas, setUpazilas] = useState([]);
+  //should be memoized or stable
+
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
   const axiosInstance = useAxiosPublic();
+
   useEffect(() => {
-    axiosInstance
-      .get("/upazilas")
-      .then((response) => setUpazilas(response.data))
-      .catch((error) => console.error(error));
+    getData();
   }, []);
 
+  // useEffect(() => {
+  //   axiosInstance
+  //     .get("/upazila")
+  //     .then((response) => setUpazilas(response.data))
+  //     .catch((error) => console.error(error));
+  // }, []);
+
+  const getData = () => {
+    axiosInstance
+      .get("/upazila")
+      .then((response) => {
+        console.log("Data:", response);
+        setData(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log("Error:", error);
+      });
+  };
+
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: "serialCode", //access nested data with dot notation
+        header: "Serial Code",
+        size: 150,
+      },
+      {
+        accessorKey: "instituteCode",
+        header: "Institute Code",
+        size: 150,
+      },
+      {
+        accessorKey: "fieldOfficeCode", //normal accessorKey
+        header: "Field Office Code",
+        size: 200,
+      },
+      {
+        accessorKey: "upazilaOfficeName",
+        header: "Upazila Office Name",
+        size: 150,
+      },
+    ],
+    []
+  );
+
+  // const table = useMaterialReactTable({
+  //   columns,
+  //   data, //data must be memoized or stable (useState, useMemo, defined outside of this component, etc.)
+  // });
+
   return (
-    <div>
-      <h2>All Upazilas</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Upazila ID</th>
-            <th>Upazila Name</th>
-            <th>Economic Codes</th>
-          </tr>
-        </thead>
-        <tbody>
-          {upazilas.map((upazila) => (
-            <tr key={upazila.id}>
-              <td>{upazila.id}</td>
-              <td>{upazila.name}</td>
-              <td>
-                {upazila.economicCodes.map((code) => (
-                  <div key={code.code}>
-                    <span>
-                      {code.code}: {code.distributedBudget}
-                    </span>
-                  </div>
-                ))}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+    <>{!loading && <MaterialReactTable columns={columns} data={data} />}</>
   );
 };
 
