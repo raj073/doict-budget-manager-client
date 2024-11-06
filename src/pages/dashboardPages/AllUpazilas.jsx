90cf45a97dde4a7e47fdb31155480fc611ce862e
raj073 <rajoni.das@outlook.com>
2024-11-07 01:00:11 +0600

    allUpazilaBudgetDataTable updated

--- src/pages/dashboardPages/AllUpazilas.jsx
+++ src/pages/dashboardPages/AllUpazilas.jsx
@@ -5,55 +5,6 @@ import {
 } from "material-react-table";
 import useAxiosPublic from "../../hooks/useAxios";
 
-//nested data is ok, see accessorKeys in ColumnDef below
-const data = [
-  {
-    name: {
-      firstName: "John",
-      lastName: "Doe",
-    },
-    address: "261 Erdman Ford",
-    city: "East Daphne",
-    state: "Kentucky",
-  },
-  {
-    name: {
-      firstName: "Jane",
-      lastName: "Doe",
-    },
-    address: "769 Dominic Grove",
-    city: "Columbus",
-    state: "Ohio",
-  },
-  {
-    name: {
-      firstName: "Joe",
-      lastName: "Doe",
-    },
-    address: "566 Brakus Inlet",
-    city: "South Linda",
-    state: "West Virginia",
-  },
-  {
-    name: {
-      firstName: "Kevin",
-      lastName: "Vandy",
-    },
-    address: "722 Emie Stream",
-    city: "Lincoln",
-    state: "Nebraska",
-  },
-  {
-    name: {
-      firstName: "Joshua",
-      lastName: "Rolluffs",
-    },
-    address: "32188 Larkin Turnpike",
-    city: "Charleston",
-    state: "South Carolina",
-  },
-];
-
 const AllUpazilas = () => {
   //should be memoized or stable
 
