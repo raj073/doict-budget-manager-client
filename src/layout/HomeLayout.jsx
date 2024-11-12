d221f3e102936ca8727472d2f086ce645c21d79c
hassandev <murad.dub@gmail.com>
2024-11-12 16:48:06 +0600

    some ui fixed

--- src/layout/HomeLayout.jsx
+++ src/layout/HomeLayout.jsx
@@ -5,19 +5,22 @@ import Navbar from "../components/Navbar";
 const HomeLayout = () => {
   return (
     <>
-      <div className="w-full lg:w-3/4 mx-auto flex flex-col min-h-screen font-popppins ">
-        <header>
-          <Navbar />
-        </header>
-        <hr />
-        <main className=" py-10">
-          <Outlet />
-        </main>
-        <hr />
+      <div className="bg-gradient-to-r from-cyan-100 via-cyan-50 to-blue-50">
+        <div className="w-full lg:w-3/4 mx-auto flex flex-col min-h-screen font-popppins ">
+          <header>
+            <Navbar />
+          </header>
+          <hr />
+          <main className=" py-10">
+            <Outlet />
+          </main>
+          <hr />
+        </div>
+        <footer className="shadow-xl">
+          <Footer />
+        </footer>
       </div>
-      <footer className="bg-base-100 shadow-xl">
-        <Footer />
-      </footer>
+
     </>
   );
 };
